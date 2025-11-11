701067dc699dbbe498716afdcd3fea1f4a6ebc34
Kehvarl <Kehvarl@Kehvarl.com>
2025-11-10 20:32:31 -0700

    feature: ranch partitioning

--- app/ranch.rb
+++ app/ranch.rb
@@ -2,8 +2,35 @@ class Ranch
     def initialize
         @dragons = []
         @inventory = []
+        @regions = [{x:0, y:0, w:1280, h:720}]
         @weather = nil
         @time_of_day = nil
+        while @regions.size < 3
+            bsp()
+        end
+    end
+
+    def setup
+    end
+
+    def bsp
+        regions = []
+        for r in @regions
+            mode = rand(4)
+            if mode == 0
+                nx = Numeric.rand((r.x+16)..(r.x+r.w-16))
+                puts(nx)
+                regions << {x:r.x, y:r.y, w:(nx-r.x), h:r.h}
+                regions << {x:nx, y:r.y, w:(r.w-(nx-r.x)), h:r.h}
+            elsif mode == 1
+                ny = Numeric.rand((r.y+16)..(r.y+r.h-16))
+                regions << {x:r.x, y:r.y, w:r.w, h:(ny-r.y)}
+                regions << {x:r.x, y:ny, w:r.w, h:(r.h-(ny-r.y))}
+            else
+                regions << {x:r.x, y:r.y, w:r.w, h:r.h}
+            end
+        end
+        @regions = regions
     end
 
     def new_dragon dragon
@@ -15,6 +42,11 @@ class Ranch
     end
 
     def render
-        @dragons
+        out = []
+        for r in @regions
+            out << {x:r.x, y:r.y, w:r.w, h:r.h, r:128, g:0, b:0}.border!
+        end
+        out << @dragons
+        out
     end
 end
