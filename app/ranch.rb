85628ac5bff70973a8315fb19b6935f22609f628
Kehvarl <Kehvarl@Kehvarl.com>
2025-11-12 18:55:51 -0700

    some colors and biomes and some biome rules

--- app/ranch.rb
+++ app/ranch.rb
@@ -1,8 +1,26 @@
 class Ranch
+    BIOMES = [:meadow, :forest, :volcanic, :cavern, :aquatic]
+    BIOME_COLORS = {
+        meadow:{r:0,g:64,b:64},
+        forest:{r:0,g:192,b:64},
+        volcanic:{r:192,g:32,b:32},
+        cavern:{r:64,g:64,b:64},
+        aquatic:{r:0,g:64,b:192},
+    }
+
+    def adjacent_biomes(biome)
+    i = BIOMES.index(biome)
+    [
+        BIOMES[(i - 1) % BIOMES.length],
+        BIOMES[i],
+        BIOMES[(i + 1) % BIOMES.length]
+    ]
+    end
+
     def initialize
         @dragons = []
         @inventory = []
-        @regions = [{x:0, y:0, w:1280, h:720}]
+        @regions = [{x:0, y:0, w:1280, h:720, biome: :meadow}]
         @weather = nil
         @time_of_day = nil
         while @regions.size < 3
@@ -14,6 +32,7 @@ class Ranch
     def setup
     end
 
+
     def bsp min_size=128
         regions = []
         for r in @regions
@@ -21,21 +40,21 @@ class Ranch
             if mode == 0
                 nx = Numeric.rand((r.x+16)..(r.x+r.w-16))
                 if (nx - r.x) < min_size or (r.w - (nx-r.x)) < min_size
-                    regions << {x:r.x, y:r.y, w:r.w, h:r.h}
+                    regions << r
                     next
                 end
-                regions << {x:r.x, y:r.y, w:(nx-r.x), h:r.h}
-                regions << {x:nx, y:r.y, w:(r.w-(nx-r.x)), h:r.h}
+                regions << {x:r.x, y:r.y, w:(nx-r.x), h:r.h, biome:adjacent_biomes(r.biome).sample()}
+                regions << {x:nx, y:r.y, w:(r.w-(nx-r.x)), h:r.h, biome:adjacent_biomes(r.biome).sample()}
             elsif mode == 1
                 ny = Numeric.rand((r.y+16)..(r.y+r.h-16))
                 if (ny - r.y) < min_size or (r.h - (ny-r.y)) < min_size
-                    regions << {x:r.x, y:r.y, w:r.w, h:r.h}
+                    regions << r
                     next
                 end
-                regions << {x:r.x, y:r.y, w:r.w, h:(ny-r.y)}
-                regions << {x:r.x, y:ny, w:r.w, h:(r.h-(ny-r.y))}
+                regions << {x:r.x, y:r.y, w:r.w, h:(ny-r.y), biome:adjacent_biomes(r.biome).sample()}
+                regions << {x:r.x, y:ny, w:r.w, h:(r.h-(ny-r.y)), biome:adjacent_biomes(r.biome).sample()}
             else
-                regions << {x:r.x, y:r.y, w:r.w, h:r.h}
+                regions << r
             end
         end
         @regions = regions
@@ -44,8 +63,8 @@ class Ranch
     def placement_grid
         for r in @regions
             r.grid = []
-            (r.x + 16).step(r.x + r.w - 32, 64).each do |x|
-                (r.y + 16).step(r.y + r.h - 32, 64).each do |y|
+            (r.x + 32).step(r.x + r.w - 32, 64).each do |x|
+                (r.y + 32).step(r.y + r.h - 32, 64).each do |y|
                     r.grid << [x + Numeric.rand(-8..8), y + Numeric.rand(-8..8)]
                 end
             end
@@ -53,15 +72,19 @@ class Ranch
     end
 
     def new_dragon dragon
-        region = @regions.sample
-        while region.grid.size <= 0
-            region = @regions.sample
+        available_regions = @regions.select { |r| r[:grid].any? }
+        if not available_regions.empty?
+
+            region = available_regions.sample
+            while region.grid.size <= 0
+                region = @regions.sample
+            end
+            pos = region.grid.sample()
+            region.grid.delete(pos)
+            dragon.x = pos[0]
+            dragon.y = pos[1]
+            @dragons << dragon
         end
-        pos = region.grid.sample()
-        region.grid.delete(pos)
-        dragon.x = pos[0]
-        dragon.y = pos[1]
-        @dragons << dragon
     end
 
     def tick args
@@ -72,6 +95,7 @@ class Ranch
         out = []
         for r in @regions
             out << {x:r.x, y:r.y, w:r.w, h:r.h, r:128, g:0, b:0}.border!
+            out << {x:r.x, y:r.y, w:r.w, h:r.h, **BIOME_COLORS[r.biome], a:32}.solid!
         end
         out << @dragons
         out
